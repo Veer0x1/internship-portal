33f7e3d7e7db6d871727a2bf5982cb61b11614e4
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-02-04 12:46:34 +0530

    nextui setup

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { CssBaseline } from "@nextui-org/react";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
+  }
+}
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>{CssBaseline.flush()}</Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
